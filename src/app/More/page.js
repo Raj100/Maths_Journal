efd252d3169b381483c3247e43fcce723653d036
Raj Rawariya <98628762+Raj100@users.noreply.github.com>
2024-05-15 19:20:27 +0530

    maths

--- src/app/More/page.js
+++ src/app/More/page.js
@@ -1,10 +1,13 @@
+'use client';
 import React from 'react'
-
+import Navbar from '../../components/Navbar/Navbar'
 const page = () => {
   return (
+    <><Navbar></Navbar>
     <div className='h-screen w-full flex items-center justify-center'>
     <h1>Coming Soon...</h1>
     </div>
+    </>
   )
 }
 
